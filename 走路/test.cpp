5ff4a9454d445fc0df57ea4c1fd262d64d95434f
changhao <1141268679@qq.com>
2020-09-20 22:25:06 +0800

    机器人走路范围

---
+++ 走路/test.cpp
@@ -0,0 +1,38 @@
+//地上有一个m行和n列的方格。一个机器人从坐标0, 0的格子开始移动，
+//每一次只能向左，右，上，下四个方向移动一格，
+//但是不能进入行坐标和列坐标的数位之和大于k的格子。 
+//例如，当k为18时，机器人能够进入方格（35, 37），因为3 + 5 + 3 + 7 = 18。
+//但是，它不能进入方格（35, 38），因为3 + 5 + 3 + 8 = 19。请问该机器人能够达到多少个格子？
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+
+    int f(int x)
+    {
+        int a = 0;
+        while (x)
+        {
+            a = a + x % 10;
+            x /= 10;
+        }
+        return a;
+    }
+
+    int movingCount(int threshold, int rows, int cols)
+    {
+        int ret = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (f(i) + f(j) <= threshold)
+                    ++ret;
+                else if (rows == 1 || cols == 1)
+                    return ret;
+            }
+        }
+        return ret;
+    }
+};
